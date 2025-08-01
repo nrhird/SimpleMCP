ff5ab1c3b977ab0957dfda99043d0da3628ae7cd
Nick Hird <nrhird@live.com>
2025-08-01 06:57:07 -0400

    added addition and subtraction mcp methods

--- SimpleMCP.Server/Program.cs
+++ SimpleMCP.Server/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace SimpleMCP.Server
+﻿using MCPSharp;
+
+namespace SimpleMCP.Server
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MCPServer.Register<CalculatorTool>();
+
+            await MCPServer.StartAsync(
+                serverName: "SimpleMCP Server", 
+                version: "v0.00.001");
+        }
+
+        public class CalculatorTool
+        {
+            [McpTool(name: "addition", Description = "This tool adds two numbers")]
+            public static int Addition(
+                [McpParameter(required:true, description: "The first number")] int firstNumber,
+                [McpParameter(required:true, description: "The second number")] int secondNumber) 
+            {
+                return firstNumber + secondNumber;
+            }
+
+            [McpTool(name: "subtraction", Description = "This tool subtracts two numbers")]
+            public static int Subtraction(
+                [McpParameter(required: true, description: "The first number")] int firstNumber,
+                [McpParameter(required: true, description: "The second number")] int secondNumber)
+            {
+                return firstNumber - secondNumber;
+            }
         }
     }
 }
