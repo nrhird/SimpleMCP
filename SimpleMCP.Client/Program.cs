7fe6840db9a4a69fa4c475074edf85e4e82aed40
Nick Hird <nrhird@live.com>
2025-08-01 00:03:20 -0400

    project setup

---
+++ SimpleMCP.Client/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleMCP.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
