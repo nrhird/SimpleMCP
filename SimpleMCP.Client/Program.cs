fc38270c342056e31daaf37496f236bb43d49f96
Nick Hird <nrhird@live.com>
2025-08-03 08:50:46 -0400

    added client to call the mcp server

--- SimpleMCP.Client/Program.cs
+++ SimpleMCP.Client/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace SimpleMCP.Client
+﻿using MCPSharp;
+using MCPSharp.Model;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace SimpleMCP.Client
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var client = new MCPClient(
+                name: "SimpleMCP Client",
+                version: "v0.00.001",
+                server: "..\\SimpleMCP.Server\\bin\\Debug\\net9.0\\SimpleMCP.Server.exe"
+            );
+
+            var resultA = await client.CallToolAsync(
+                name: "addition",
+                parameters: new Dictionary<string, object> 
+                {
+                    { "firstNumber", 5 },
+                    { "secondNumber", 10 }
+                }
+            );
+            Console.WriteLine(resultA.Content[0].Text);
+
+            var resultS = await client.CallToolAsync(
+                name: "subtraction",
+                parameters: new Dictionary<string, object>
+                {
+                    { "firstNumber", 15 },
+                    { "secondNumber", 10 }
+                }
+            );
+            Console.WriteLine(resultS.Content[0].Text);
+
         }
     }
 }
